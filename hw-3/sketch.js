186a2ff3c6fba784cef60e708c783409d2896478
Michael <31572980+mjohnson92@users.noreply.github.com>
2017-09-27 18:40:16 -0600

    pt 1
    pt 1

--- hw-3/sketch.js
+++ hw-3/sketch.js
@@ -1,9 +1,15 @@
 // Declare the setup function
 function setup() {
 
-    // create a canvas 600px wide and 400px high
+// create a canvas 600px wide and 400px high
     createCanvas( 600, 400 );
 
-    // color the background 'pink'
+// color the background 'pink'
     background( 'pink' );
+}
+
+// Declare the draw function
+function draw() {
+
+    text( "Hello World!", 20, 30 );
 }
