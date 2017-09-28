b9748022124d82705f0a87a468e58db88c8d0c8f
Michael <31572980+mjohnson92@users.noreply.github.com>
2017-09-27 18:36:51 -0600

    z
    z

---
+++ hw-3/sketch.js
@@ -0,0 +1,9 @@
+// Declare the setup function
+function setup() {
+
+    // create a canvas 600px wide and 400px high
+    createCanvas( 600, 400 );
+
+    // color the background 'pink'
+    background( 'pink' );
+}
